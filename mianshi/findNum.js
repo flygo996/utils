c8bf54fdd928750c19e031fd647ad168ec85536b
laifeipeng <1020103647@qq.com>
2019-02-21 14:48:14 +0800

    增加查找数组元素的优化方法

---
+++ mianshi/findNum.js
@@ -0,0 +1,61 @@
+/*
+ * @Author: laifeipeng 
+ * @Date: 2019-02-20 17:36:01 
+ * @Last Modified by: laifeipeng
+ * @Last Modified time: 2019-02-21 14:46:18
+ */
+// 给一个数组（其元素不重复），求所有元素相加为某个值的2个元素对的下标对
+// eg: [2, 9, 3, 10, 8, 1, 22] 目标值11，有[[0, 1], [2, 4], [3, 5]]
+function findNum(arr, target) {
+  const a = arr.slice().sort((a, b) => a - b);
+  const len = a.length;
+  const key = [];//存放最后的元素组
+  const val = [];//存放最后的下标组
+  if (len <= 1) return;
+  let i = 0;
+  let j = len - 1;
+  while (i < j) {
+    if (a[i] + a[j] < target) {
+      i++
+    } else if (a[i] + a[j] > target) {
+      j--
+    } else {
+      val.push([a[i], a[j]])
+      key.push([i, j])
+      i++; j--;
+    }
+  }
+  // 如果只要下标组，则：return key;
+  return {
+    key, val
+  }
+}
+
+// 上面的算法实现需要排序数组，下面介绍一个不用排序的算法
+function findNum2(arr, target) {
+  const a = arr.slice().sort((a, b) => a - b);
+  const len = a.length;
+  const key = [];//存放最后的元素组
+  const val = [];//存放最后的下标组
+  if (len <= 1) return;
+  for (let i = 0; i < len; i++) {
+    const diff = target - a[i];
+    // 从i+1开始查询是否存在diff的值（下标）
+    const index = a.slice(i + 1).findIndex(i => i === diff);
+    if (index !== -1) {
+      key.push([i,index]);
+      val.push([a[i],a[index]]);
+    }
+  }  
+  // 如果只要下标组，则：return key;
+  return {
+    key, val
+  }
+}
+
+const arr = [2, 9, 3, 10, 8, 1, 22];
+const target = 11;
+console.log(findNum(arr, target))
+console.log(findNum2(arr, target))
+// { key: [ [ 0, 5 ], [ 1, 4 ], [ 2, 3 ] ],
+//   val: [ [ 1, 10 ], [ 2, 9 ], [ 3, 8 ] ] }
