ad3ec510c59251dbfe9cce8a923105c63c517213
laifeipeng <1020103647@qq.com>
2019-02-24 11:35:15 +0800

    jsonp的注释优化

--- jsonp/promise.js
+++ jsonp/promise.js
@@ -2,7 +2,7 @@
  * @Author: laifeipeng
  * @Date: 2019-02-24 11:05:45
  * @Last Modified by: laifeipeng
- * @Last Modified time: 2019-02-24 11:06:56
+ * @Last Modified time: 2019-02-24 11:22:27
  */
 /**
 一句话阐述下JSONP原理：
@@ -14,21 +14,23 @@
 3.不支持post，因为js标签本身就是一个get请求
 */
 
-const jsonp = function (url, data) {
+function jsonp(url, data) {
   return new Promise((resolve, reject) => {
-    // 初始化url
+    // 1、初始化url
     let dataString = url.indexOf('?') === -1 ? '?' : '&'
     let callbackName = `jsonpCB_${Date.now()}`
     url += `${dataString}callback=${callbackName}`
     if (data) {
-     // 有请求参数，依次添加到url
+      // 有请求参数，依次添加到url
       for (let k in data) {
         url += `&${k}=${data[k]}`
       }
     }
+    // 2、动态生成js标签
     let jsNode = document.createElement('script')
     jsNode.src = url
-    // 触发callback，触发后删除js标签和绑定在window上的callback
+
+    // 3、触发callback，触发后删除js标签和绑定在window上的callback
     window[callbackName] = result => {
       delete window[callbackName]
       document.body.removeChild(jsNode)
@@ -38,16 +40,18 @@ const jsonp = function (url, data) {
         reject('没有返回数据')
       }
     }
-    // js加载异常的情况
+    // 4、js加载异常的情况
     jsNode.addEventListener('error', () => {
       delete window[callbackName]
       document.body.removeChild(jsNode)
       reject('JavaScript资源加载失败')
     }, false)
-    // 添加js节点到document上时，开始请求
+
+    // 5、添加js节点到document上时，开始请求
     document.body.appendChild(jsNode)
   })
 }
-jsonp('http://192.168.0.103:8081/jsonp', {a: 1, b: 'heiheihei'})
+// 测试
+jsonp('http://127.0.0.1:3000/jsonp', { a: 1, b: 'heiheihei' })
   .then(result => { console.log(result) })
   .catch(err => { console.error(err) })
