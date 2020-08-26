f876745134f6044cc73f3660b52bb41a601fb60d
pigeon <1390124183@qq.com>
2020-08-26 18:28:05 +0800

    trondice 测试

---
+++ tronDice-test/records.js
@@ -0,0 +1,27 @@
+const accounts = {
+  101515: {
+    refCode: '101515',
+    referrer: ['0000000000000000000000000000000000000000000000000000000000000000'], // 64位
+    base58: 'TVyKcTBPrqF6PPT1m2vqZems1ntm3J8d5y',
+    hex: '41db68089b0510b3e708f1c6720baa327f5923176e', // 42位(2+40)
+  },
+  f111: {
+    refCode: 'f111',
+    referrer: '',
+    base58: 'TFe7FdBWwGWEredjSBpvpZ4CNudxQq94xW',
+    hex: '413e340d719f8f716dc65d88d4e53cad89007252e3',
+  },
+  f222: {
+    mentor: '101515',
+    refCode: 'f222',
+    referrer: ['000000000000000000000000db68089b0510b3e708f1c6720baa327f5923176e'], // 64位(24+40)
+    base58: 'TLmvs4aYyj6WCFUCeGxrqVSue3SepZitWY',
+    hex: '417687563a3b444ff30bf77b2ad0381f33472d1111',
+  },
+  test1: {
+    refCode: '',
+    referrer: ['0000000000000000000000000000000000000000000000000000000000000000'],
+    base58: 'TNFsJTpa2JzQFqAeTnm58meYTZAQodNKfd',
+    hex: '4186c822d674ab311be9a619aacafa4475290553ce',
+  },
+};
