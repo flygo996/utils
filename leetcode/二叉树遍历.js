e4fec0d0e1e3246a7124af35293cd0109c23e59a
laifeipeng <1020103647@qq.com>
2020-09-06 18:41:09 +0800

    二叉树的迭代解法

--- leetcode/二叉树遍历.js
+++ leetcode/二叉树遍历.js
@@ -53,3 +53,20 @@ function threeOrders (root) {
   postTraversal(root)
   return [pre, mid, post]
 }
+
+// 先序遍历--迭代解法：
+/**
+ * @param {TreeNode} root
+ * @return {arr[]}
+ */
+var preorderTraversal = function (root) {
+  let stack = [root];
+  let arr = [];
+  while (stack.length > 0) {//循环迭代
+      let node = stack.pop();
+      node && arr.push(node.val); // node不为空时，向arr中推入节点值
+      node && node.right && stack.push(node.right); //关键点：模拟栈，后入先出，故先压右节点
+      node && node.left && stack.push(node.left); // 关键点：后入先出，后压左节点
+  }
+  return arr
+};
