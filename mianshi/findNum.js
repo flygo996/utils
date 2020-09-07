0e13ae6b02077aed627c0a35aa7a51c2f0b0bd35
laifeipeng <1020103647@qq.com>
2020-09-07 15:07:05 +0800

    opt

--- mianshi/findNum.js
+++ mianshi/findNum.js
@@ -1,22 +1,22 @@
 /*
- * @Author: laifeipeng 
- * @Date: 2019-02-20 17:36:01 
+ * @Author: laifeipeng
+ * @Date: 2019-02-20 17:36:01
  * @Last Modified by: laifeipeng
- * @Last Modified time: 2019-02-22 12:19:27
+ * @Last Modified time: 2020-09-07 15:06:44
  */
 // 给一个数组（其元素不重复），求所有元素相加为某个值的2个元素对的下标对
 // eg: [2, 9, 3, 10, 8, 1, 22] 目标值11，有[[0, 1], [2, 4], [3, 5]]
 
 // 本人的实现不仅找出下标组，把对应的元素组也一起返回了，提高函数功能，谁知道后面会不会更改需求呢
 
-function findNum(arr, target) {
-  const a = arr.slice().sort((a, b) => a - b);
-  const len = a.length;
-  const key = [];//存放最后的元素组
-  const val = [];//存放最后的下标组
-  if (len <= 1) return;
-  let i = 0;
-  let j = len - 1;
+function findNum (arr, target) {
+  const a = arr.slice().sort((a, b) => a - b)
+  const len = a.length
+  const key = [] //存放最后的元素组
+  const val = [] //存放最后的下标组
+  if (len <= 1) return
+  let i = 0
+  let j = len - 1
   while (i < j) {
     if (a[i] + a[j] < target) {
       i++
@@ -25,39 +25,43 @@ function findNum(arr, target) {
     } else {
       val.push([a[i], a[j]])
       key.push([i, j])
-      i++; j--;
+      i++
+      j--
     }
   }
   // 如果只要下标组，则：return key;
   return {
-    key, val
+    key,
+    val
   }
 }
 
 // 上面的算法实现需要排序数组，下面介绍一个不用排序的算法
-function findNum2(arr, target) {
-  const len = arr.length;
-  const key = [];//存放最后的元素组
-  const val = [];//存放最后的下标组
-  if (len <= 1) return;
-  for (let i = 0; i < len; i++) {
-    const diff = target - arr[i];
+function findNum2 (arr, target) {
+  const len = arr.length
+  const key = [] //存放最后的元素组
+  const val = [] //存放最后的下标组
+  if (len <= 1) return
+  // 注意不是i>=0
+  for (let i = len; i > 0; i--) {
+    const diff = target - arr[i]
     // 从i+1开始查询是否存在diff的值（下标）
-    const index = arr.slice(i + 1).findIndex(i => i === diff);
+    const index = arr.slice(0, i).indexOf(diff)
     if (index !== -1) {
-      key.push([i,index]);
-      val.push([arr[i],arr[index]]);
+      key.push([i, index])
+      val.push([arr[i], arr[index]])
     }
-  }  
+  }
   // 如果只要下标组，则：return key;
   return {
-    key, val
+    key,
+    val
   }
 }
 
 // 下面是测试
-const arr = [2, 9, 3, 10, 8, 1, 22];
-const target = 11;
+const arr = [2, 9, 3, 10, 8, 1, 22]
+const target = 11
 console.log(findNum(arr, target))
 console.log(findNum2(arr, target))
 // { key: [ [ 0, 5 ], [ 1, 4 ], [ 2, 3 ] ],
