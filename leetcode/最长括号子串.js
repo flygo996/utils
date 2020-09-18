f1953e5a7488e40e1b997f43edf95ab61c9b130a
laifeipeng <1020103647@qq.com>
2020-09-18 10:53:13 +0800

    opt

--- leetcode/最长括号子串.js
+++ leetcode/最长括号子串.js
@@ -27,11 +27,12 @@ function longestValidParentheses (s) {
     return 0
   }
   let res = 0
+  // dp[i]是i位置的合法的最长长度
   const dp = Array(s.length).fill(0) // 一定要先设置默认值0！！
   for (let i = 1; i < s.length; i++) {
     // 如果是'('直接跳过，（此时dp[i]还是为默认值0）
     if (s[i] == ')') {
-      const prev = i - 1 - dp[i - 1] // dp[i-1]是合法的最长长度，i-1前1个下标，i-1-dp[i-1]（即prev）是上一个最长合法的前一个下标
+      const prev = i - 1 - dp[i - 1] // dp[i-1]是合法的最长长度，i-1是i的前1个下标，i-1-dp[i-1]是上一个最长合法的前一个下标
       if (prev >= 0 && s[prev] == '(') {
         dp[i] = dp[i - 1] + 2
         if (prev - 1 >= 0) {
