8b7ee96c6ab3bd952bb2c7c73df32b5a6cb0c3e9
laifeipeng <1020103647@qq.com>
2020-09-27 23:40:20 +0800

    异步

--- js-coding/async/2.js
+++ js-coding/async/2.js
@@ -16,6 +16,10 @@ function handle(x) {
 }
 
 test()
+// 结束
+// 1
+// 2
+// 4
 
 // http://47.98.159.95/my_blog/js-async/012.html#%E8%A7%A3%E5%86%B3%E5%8E%9F%E7%90%86%E2%80%94%E2%80%94iterator
 // 其实也很简单, 我们利用for...of就能轻松解决。
@@ -29,3 +33,7 @@ async function test2() {
 }
 
 test2()
+// 4
+// 2
+// 1
+// 结束
