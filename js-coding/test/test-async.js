d0359fabdce571f6d5d502747cc2ae60ac52f348
laifeipeng <1020103647@qq.com>
2020-10-13 22:01:17 +0800

    test async

---
+++ js-coding/test/test-async.js
@@ -0,0 +1,42 @@
+async function async1 () {
+  console.log('async1 start')
+  await async2()
+  console.log('async1 end')
+}
+async function async2 () {
+  console.log('async2')
+}
+console.log('script start')
+setTimeout(function () {
+  console.log('setTimeout')
+}, 0)
+
+async1()
+new Promise(function (resolve) {
+  console.log('promise1')
+  resolve()
+}).then(function () {
+  console.log('promise2')
+})
+console.log('script end')
+
+
+// 浏览器输出
+// script start
+// async1 start
+// async2
+// promise1
+// script end
+// async1 end
+// promise2
+// setTimeout
+
+// node环境输出
+// script start
+// async1 start
+// async2
+// promise1
+// script end
+// promise2
+// async1 end
+// setTimeout
